7666f76a927dbaa4f4cbc5b51e4f2c4b0addb24d
root <root@ccSurface.localdomain>
2020-11-14 13:04:16 -0500

    First Commit

---
+++ p2/frontend/src/components/math/mathSubjects.js
@@ -0,0 +1,24 @@
+export default {
+	mathSubjects: {
+		add: {
+			name: "Addition",
+			link: "/math/practice/addition",
+		},
+		subtract: {
+			name: "Subtraction",
+			link: "/math/practice/subtraction",
+		},
+		multiply: {
+			name: "Multiplication",
+			link: "/math/practice/multiplication",
+		},
+		exponents: {
+			name: "Exponent",
+			link: "/math/practice/exponents",
+		},
+		division: {
+			name: "Division",
+			link: "/math/practice/division",
+		},
+	},
+}
---
+++ p2/frontend/src/views/flashCards/EditFlashCardsHome.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="container flex flex-col justify-center flex-grow-0 mx-auto">
+		<p class="font-semibold text-center">Your Flash Cards</p>
+		<div class="max-w-xl mx-auto ">
+			<ol class="list-decimal list-inside">
+				<li
+					class="py-1"
+					v-for="(flashCard, index) in flashCards"
+					:key="index"
+				>
+					<router-link
+						class="hover:text-blue-600"
+						:to="
+							`/flashcards/editflashcards/${index}`
+						"
+					>
+						{{ flashCard.question }}
+					</router-link>
+				</li>
+			</ol>
+		</div>
+	</div>
+</template>
+
+<script>
+	import { inject } from "vue"
+	export default {
+		setup() {
+			const flashCards = inject("flashCards")
+			return { flashCards }
+		},
+	}
+</script>
+
+<style></style>
